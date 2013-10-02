4a304c1f7164b6ac06d44d610151d7c93c218567
Ryan Floyd <ryanfloyd@live.com>
2013-10-01 17:25:14 -0700

    V2

--- mosaic.py
+++ mosaic.py
@@ -1,7 +1,9 @@
 import urllib
 import sys
+import os
 import json
 from urllib import FancyURLopener
+from PIL import Image
 import itertools
 import optparse
 import osaic
@@ -42,15 +44,12 @@ def downloadImage(prefix, url, i):
     myopener = MyOpener()
     localFileName = "images/%s-%s.jpg" % (prefix, i)
     try:
-        print "Downloading: %s to: %s" % (url, localFileName)
+        print "D: %s to: %s" % (url, localFileName)
         myopener.retrieve(url, localFileName)
     except Exception, err:
+        os.remove(filename)
         sys.stderr.write('ERROR: %s\n' % str(err))
         return None
-    if (imghdr.what('jpg', localFileName)):
-        print "Image %s isn't valid" % localFileName
-        return None
-    print localFileName
     return localFileName
 
 
@@ -62,32 +61,70 @@ def createMosaic(filename, sources, target):
         zoom=4,
         output=filename,
         )
+    print "Success"
 
 
 if __name__=='__main__':
     # Parse Inputs. Optparse makes it really easy
     parser = optparse.OptionParser()
+    parser.add_option("-a", "--action",
+                  action="store", type="string", dest="action", help="Specify the action: download or compose")
     parser.add_option("-f", "--file",
                   action="store", type="string", dest="filename", help="The name of the output file")
+    parser.add_option("-t", "--target",
+                  action="store", type="string", dest="targetfilename", help="The name of the input file to mosaicify")
     parser.add_option("-n", type="int", dest="num", help="How many images to download from Google Images")
     parser.add_option("-q", "--query", action="store", type="string", dest="query", help="Search Term string, ex: automobile")
+    parser.add_option("-s", "--sourceDir", action="store", type="string", dest="sourceDir", help="Source Directory, will skip downloading from Google")
     (opts, args) = parser.parse_args()
 
     outputFileName = "mosaicFinal.jpg" if (opts.filename is None) else opts.filename
     numIterations = 50 if (opts.num is None) else opts.num
-
-    if (opts.query is None):
+    action = "compose"
+    if(opts.action is None):
+        print "Action argument required"
+        parser.print_help()
+        exit(-1)
+    else:
+        action = opts.action.lower()
+    if(not (action == "download" or action == "compose")):
+        print "Action argument should be either download or compose"
+        parser.print_help()
+        exit(-1)
+    if (action == "download" and opts.query is None):
         print "Query Argument is missing, please specify a search term to query Google images\n"
         parser.print_help()
         exit(-1)
+    if (action == "compose" and opts.targetfilename is None):
+        print "Target filename is missing, please specify a target image to mosaicify\n"
+        parser.print_help()
+        exit(-1)
 
-    # Retrieve list of images from Google AJAX API Image Search
-    urls = showmore(opts.query, numIterations)
-
-    # Download all images and store in /images directory
     localImages = []
-    for i, image in enumerate(urls):
-        localImages.append(downloadImage(opts.query.replace(' ', '-'), image, i))
+    if(action == "download"):
+        # Retrieve list of images from Google AJAX API Image Search
+        urls = showmore(opts.query, numIterations)
 
-    # Create Mosaic
-    createMosaic(outputFileName, localImages, 'images/bicycle-3.jpg')
+        # Download all images and store in /images directory
+        for i, image in enumerate(urls):
+            filename = downloadImage(opts.query.replace(' ', '-'), image, i)
+            try:
+                im = Image.open(filename)
+                im.verify()
+                localImages.append(filename)
+            except Exception, err:
+                sys.stderr.write('ERROR: %s: %s\n' % (filename, str(err)))
+
+    if(action  == "compose"):
+        #Get Images from SourceDIr
+        for f in os.listdir(opts.sourceDir):
+            filename = os.path.join(opts.sourceDir, f)
+            try:
+                im = Image.open(filename)
+                im.verify()
+                localImages.append(filename)
+            except Exception, err:
+                sys.stderr.write('ERROR: %s: %s\n' % (filename, str(err)))
+
+        # Create Mosaic
+        createMosaic(outputFileName, localImages, opts.targetfilename)
